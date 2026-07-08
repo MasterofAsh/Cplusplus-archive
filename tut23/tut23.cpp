1ba0256a388532fb054ef1ba8b0ddc4218fc819d
Master of Ash <aa4070197@gmail.com>
2026-07-08 10:37:00 +0500

    23rd Lesson

---
+++ tut23/tut23.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Employee{
+
+    int id;
+    int salary;
+    public:
+        void setID(void){
+            salary = 122;
+            cout<<"Enter the ID of employee: "<<endl;
+            cin>>id;
+        }
+        void getID(void){
+            cout<<"The ID of this employee is: "<<id<<endl;
+        }
+
+
+};
+
+
+int main() {
+    Employee FB[4];
+    for (int i = 0; i < 4; i++)
+    {
+        FB[i].setID();
+        FB[i].getID();
+    }
+    
+   
+    return 0;
+}
